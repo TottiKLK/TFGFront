67cb34f1c8d2f7391b755c2e9f8c83ffcdc003c7
DESKTOP-ARDGTBB\ikert <a22782@svalero.com>
2024-04-25 09:04:24 +0200

    Commit de prueba

---
+++ src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+    <nav>
+        <div class="logo">
+            <img src="" alt="">
+        </div>
+        <ul>
+            <li><router-link to="/">HOME</router-link></li>
+            <li><router-link to="/">HOME</router-link></li>
+            <li><router-link to="/">HOME</router-link></li>
+            <li><router-link to="/">HOME</router-link></li>
+        </ul>
+    </nav>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'Navbar',
+}
+</script>
+
+<style scoped>
+nav {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 1em;
+}
+
+ul {
+    list-style: none;
+    display: flex;
+    gap: 1em;
+}
+</style>
