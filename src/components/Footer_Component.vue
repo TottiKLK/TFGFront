8c2be35dd9c6360cb85f0851a96481c443a84fa6
DESKTOP-ARDGTBB\ikert <a22782@svalero.com>
2024-04-30 10:04:06 +0200

    Footer casi terminado falta retocar algunas cosas

---
+++ src/components/Footer_Component.vue
@@ -0,0 +1,90 @@
+<template>
+    <footer class="footer-container">
+        <div class="footer-content">
+            <img src="../assets/logo.png" alt="Logo de la Empresa" class="footer-logo">
+            <div class="footer-links">
+                <a href="/sobrenosotros">Sobre Nosotros</a>
+                <a href="/reservas">Reservas</a>
+                <a href="/productos">Productos</a>
+                <div class="footer-info">
+                    <p>Email: contacto@padel.com</p>
+                    <p>Tel: +34 123 456 789</p>
+                    <p>Dirección: Calle Falsa 123, Madrid, España</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'FooterComponent'
+}
+</script>
+
+<style scoped>
+.footer-container {
+    background-color: #d8d8d8;
+    color: #000;
+    padding: 20px 50px;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+}
+
+.footer-content {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+    max-width: 1200px;
+    margin: auto;
+}
+
+.footer-logo {
+    height: 50px;
+}
+
+.footer-links a,
+.footer-info p {
+    color: #000;
+    text-decoration: none;
+    margin-left: 20px;
+    font-size: 16px;
+}
+
+.footer-links a:hover {
+    text-decoration: underline;
+}
+
+.footer-info {
+    text-align: center;
+    flex-basis: 100%;
+    order: 3;
+}
+
+@media (max-width: 768px) {
+    .footer-container {
+        padding: 20px;
+        flex-direction: column;
+    }
+
+    .footer-content {
+        flex-direction: column;
+    }
+
+    .footer-links,
+    .footer-info {
+        text-align: center;
+        margin-top: 10px;
+    }
+
+    .footer-links a {
+        display: block;
+        margin-left: 0;
+        margin-bottom: 10px;
+    }
+}
+</style>
--- src/views/Home_View.vue
+++ src/views/Home_View.vue
@@ -8,13 +8,15 @@
         </template>
         <template v-slot:ropa>
         </template>
-        </Products>
+    </Products>
+    <FooterComponent />
 </template>
 
 <script>
 import Navbar_Component from '../components/Navbar_Component.vue';
 import ImgHome from '../components/ImgHome_Component.vue';
 import Products from '../components/Products_Component.vue';
+import FooterComponent from '../components/Footer_Component.vue';
 
 export default {
     name: 'home_view',
@@ -22,8 +24,10 @@ export default {
         Navbar_Component,
         ImgHome,
         Products,
+        FooterComponent,
     },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+</style>
